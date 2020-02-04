3a15f85d9bd9c5ed7d96d132219bea815314d698
MaiconSlavieiro <maiconslavieiro@outlook.com>
2020-02-03 23:15:34 -0200

    Animação de logo + mudaças de cor

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function main() {
   var start_overlay_img = document.querySelector(".overlay_start_img");
   var main_title_bg_img = document.querySelector(".main_title_bg_img");
+  var logo_svg_paths = document.getElementsByClassName("cls-1");
   document.addEventListener("scroll", isScrolling);
 
   function isScrolling() {
@@ -11,6 +12,10 @@ function main() {
         (scroll_position - baseLine) * 0.3 + 100 + "%";
       main_title_bg_img.style.filter =
         "blur(" + (scroll_position - baseLine) * 0.01 + "px)";
+    }
+
+    for (var path of logo_svg_paths) {
+      path.style.strokeDasharray = scroll_position * 2.5;
     }
   }
 }
