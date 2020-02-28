2b7a3b8631b1ca2ae7dcadef2896578533b4fc8a
MaiconSlavieiro <maiconslavieiro@outlook.com>
2020-02-27 23:30:34 -0300

    Refatoração da tela inicial

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 function main() {
-  var start_overlay_img = document.querySelector(".overlay_start_img");
-  var main_title_bg_img = document.querySelector(".main_title_bg_img");
-  var main_title = document.querySelector(".main_title");
-  var logo_svg_paths = document.getElementsByClassName("cls-1");
+  var pc_img = document.querySelector(".pc_img");
+  var bg_img = document.querySelector(".bg_img");
+
   document.addEventListener("scroll", isScrolling);
 
-  function isScrolling() {
-    var baseLine = 260;
-    var scroll_position = window.scrollY;
-    if (scroll_position > baseLine) {
-      start_overlay_img.style.backgroundSize =
-        (scroll_position - baseLine) * 0.3 + 100 + "%";
-      main_title_bg_img.style.filter =
-        "blur(" + (scroll_position - baseLine) * 0.01 + "px)";
-    }
+  var maxSize = window.innerWidht > window.innerHeight ? window.innerHeight : window.innerHeight * 3;
+  bg_img.style.height = maxSize + "px";
 
-    for (var path of logo_svg_paths) {
-      path.style.strokeDasharray = scroll_position * 2.5;
+  function isScrolling() {
+    if (maxSize == window.scrollY + window.innerHeight) {
+      pc_img.classList.toggle("zoomin");
+      setTimeout(function () {
+        window.location.href = "./dir/desktop.html";
+      }, 3000);
+
     }
   }
+
+
+
 }
 
 main();
